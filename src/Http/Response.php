db46c332ce4e45c8f9466656e6bff7cc2e786ee6
Jérémy GUERIBA <jeremy.gueriba@gmail.com>
2024-09-02 01:04:52 +0200

    Implement Response class

---
+++ src/Http/Response.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http;
+
+class Response
+{
+    public const int HTTP_OK = 200;
+    public const int HTTP_NOT_FOUND = 404;
+    public const int HTTP_BAD_REQUEST = 400;
+    public const int HTTP_SERVER_ERROR = 500;
+
+    public const string CONTENT_TYPE_HTML = 'text/html';
+    public const string CONTENT_TYPE_JSON = 'application/json';
+
+    public function __construct(
+        private string $content,
+        private int $statusCode = self::HTTP_OK,
+        private string $contentType = self::CONTENT_TYPE_HTML
+    )
+    {
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): void
+    {
+        $this->content = $content;
+    }
+
+    public function getStatusCode(): int
+    {
+        return $this->statusCode;
+    }
+
+    public function setStatusCode(int $statusCode): void
+    {
+        $this->statusCode = $statusCode;
+    }
+
+    public function getContentType(): string
+    {
+        return $this->contentType;
+    }
+
+    public function setContentType(string $contentType): void
+    {
+        $this->contentType = $contentType;
+    }
+}
