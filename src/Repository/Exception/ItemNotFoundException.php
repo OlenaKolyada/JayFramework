e3398660b6d2e59890521edec03bdc3d9bc2b175
Jérémy GUERIBA <jeremy.gueriba@gmail.com>
2024-09-02 01:07:43 +0200

    Implement HttpExceptionInterface to repository exceptions

--- src/Repository/Exception/ItemNotFoundException.php
+++ src/Repository/Exception/ItemNotFoundException.php
@@ -2,12 +2,19 @@
 
 namespace App\Repository\Exception;
 
+use App\Exception\HttpExceptionInterface;
+use App\Http\Response;
+
 /**
  * Class ItemNotFoundException
  *
  * @package App\Repository\Exception
  * @author Jérémy GUERIBA
  */
-class ItemNotFoundException extends \Exception
+class ItemNotFoundException extends \Exception implements HttpExceptionInterface
 {
+    public function getStatusCode(): int
+    {
+        return Response::HTTP_NOT_FOUND;
+    }
 }
