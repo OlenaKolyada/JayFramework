53e9b231aca8ce2d08dbbdfdf671b96f7a30b54a
Jérémy GUERIBA <jeremy.gueriba@gmail.com>
2024-09-01 21:30:30 +0200

    Implement route dispatch in kernel

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,10 +2,30 @@
 
 namespace App;
 
+use App\Controller\IndexAction;
+use App\Kernel\Router;
+
+/**
+ * Class Kernel
+ *
+ * @package App
+ * @author Jérémy GUERIBA
+ */
 class Kernel
 {
     public static function run(): void
     {
+        self::initRoutes();
+    }
 
+    /**
+     * @throws Router\Exception\CallbackIsNotCallableException
+     */
+    private static function initRoutes(): void
+    {
+        $router = new Router();
+        $router->addRoute('GET', '/', new IndexAction());
+
+        $router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI'], $_SERVER['SCRIPT_NAME']);
     }
 }
