e291bbf3d07dfdee3ecba9f8a45815173d4b0caa
Jérémy GUERIBA <jeremy.gueriba@gmail.com>
2024-09-02 01:15:46 +0200

    Implement view handling in kernel

--- src/Kernel.php
+++ src/Kernel.php
@@ -28,7 +28,16 @@ class Kernel
      */
     public static function run(): void
     {
-        self::launchRouting();
+        ob_start();
+
+        try {
+            self::launchRouting();
+            self::dispatch();
+        } catch (HttpExceptionInterface $e) {
+            self::displayError($e->getStatusCode(), $e);
+        } catch (\Exception $e) {
+            self::displayError(Response::HTTP_SERVER_ERROR, $e);
+        }
     }
 
     /**
@@ -52,5 +61,38 @@ class Kernel
     private static function loadConfig(string $configFile): array
     {
         return Yaml::parseFile(self::APP_CONFIG_DIR . $configFile . '.yaml');
+    }
+
+    /**
+     * @throws Router\Exception\RouteNotFoundException
+     * @throws \Exception
+     */
+    private static function dispatch(): void
+    {
+        $result = self::$router->dispatch(
+            $_SERVER['REQUEST_METHOD'],
+            $_SERVER['REQUEST_URI'],
+            $_SERVER['SCRIPT_NAME']
+        );
+
+        ob_end_clean();
+
+        if ($result instanceof Response) {
+            header('Content-Type: ' . $result->getContentType());
+            http_response_code($result->getStatusCode());
+
+            echo $result->getContent();
+        } else if (is_string($result)) {
+            echo $result;
+        } else {
+            throw new \Exception('Something went wrong');
+        }
+    }
+
+    private static function displayError(int $statusCode, \Exception $e): void
+    {
+        header('Content-Type: ' . Response::CONTENT_TYPE_HTML);
+        http_response_code($statusCode);
+        include View::TEMPLATE_BASE_PATH . 'error.html.php';
     }
 }
