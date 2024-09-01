350066a31d8460144439fe3dea02b6b2bef8d3b8
Jérémy GUERIBA <jeremy.gueriba@gmail.com>
2024-09-01 22:35:55 +0200

    Implement model example

---
+++ src/Model/News.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Model;
+
+class News
+{
+    private int $id;
+    private string $title;
+    private string $content;
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): void
+    {
+        $this->content = $content;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+}
