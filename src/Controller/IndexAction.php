81835811181c2db12a8c67e15ad100ca2316cfd3
Jérémy GUERIBA <jeremy.gueriba@gmail.com>
2024-09-01 21:28:12 +0200

    Implement an example controller for test purpose

---
+++ src/Controller/IndexAction.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+/**
+ * Class IndexAction
+ *
+ * @package App\Controller
+ * @author Jérémy GUERIBA
+ */
+class IndexAction
+{
+    public function __invoke()
+    {
+        echo 'Hello World!';
+    }
+}
