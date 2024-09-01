5ee7335132394650f5fcf4ec8c95324ca51b6158
Jérémy GUERIBA <jeremy.gueriba@gmail.com>
2024-09-01 22:14:07 +0200

    Kernel handle defined route from yaml config file

--- src/Kernel.php
+++ src/Kernel.php
@@ -3,7 +3,11 @@
 namespace App;
 
 use App\Controller\IndexAction;
+use App\Controller\NewsCollectionAction;
+use App\Controller\NewsItemAction;
 use App\Kernel\Router;
+use APP\Kernel\Router\Exception\NoAvailableRouteException;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * Class Kernel
@@ -13,19 +17,41 @@ use App\Kernel\Router;
  */
 class Kernel
 {
+    public const string APP_ROOT_DIR = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR;
+    public const string APP_CONFIG_DIR = self::APP_ROOT_DIR . 'config' . DIRECTORY_SEPARATOR;
+
+    private static Router $router;
+
     public static function run(): void
     {
-        self::initRoutes();
+        self::launchRouting();
     }
 
     /**
+     * @throws NoAvailableRouteException
      * @throws Router\Exception\CallbackIsNotCallableException
      */
-    private static function initRoutes(): void
+    private static function launchRouting(): void
     {
-        $router = new Router();
-        $router->addRoute('GET', '/', new IndexAction());
+        self::$router = new Router();
 
-        $router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI'], $_SERVER['SCRIPT_NAME']);
+        $configuredRoutes = self::loadConfig('routes');
+        $routes = $configuredRoutes['routes'] ?? null;
+
+        if (empty($routes)) {
+            throw new NoAvailableRouteException("No routes found");
+        }
+
+        foreach ($routes as $uri => $routeConfiguration) {
+            $callable = class_exists($routeConfiguration['callable']) ? new $routeConfiguration['callable']() : $routeConfiguration['callable'];
+            self::$router->addRoute($routeConfiguration['method'], $uri, $callable);
+        }
+
+        self::$router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI'], $_SERVER['SCRIPT_NAME']);
+    }
+
+    private static function loadConfig(string $configFile): array
+    {
+        return Yaml::parseFile(self::APP_CONFIG_DIR . $configFile . '.yaml');
     }
 }
